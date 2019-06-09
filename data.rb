da0e56c32b227f35a656e459dfe4a65885b2fdcc
Angelo Batts <angelopbatts@gmail.com>
2019-06-09 19:48:01 +0000

    Automatically backed up by Learn

---
+++ data.rb
@@ -0,0 +1,50 @@
+age = 28
+height = 160
+
+
+#This is how to use variables
+puts height + age
+
+#working with String 
+first_name = "Ann"
+last_name = "Doung"
+#puts first_name + " " + last_name
+
+#puts "Ann" .length
+puts last_name.downcase
+#puts last_name.upcase
+
+teacher = "Ann"
+teacher = "Trevor"
+#teacher = "Angelo"
+
+puts teacher 
+#puts teacher1
+#puts teacher2
+#puts teacher3
+
+#How to create an Arrayteacher
+teachers = ["Ann", "Trevor","Joseph"]
+#puts teachers
+
+#How to index an array 
+# puts teachers[0]
+# puts teachers[2]
+# puts teachers[1]
+#Indexing starts at number 0
+# puts teachers
+
+numbers = [28, 160, 120]
+# puts numbers
+# puts numbers[0] + numbers[1]
+
+#What is a boolean? is a true or FALSE
+female = true
+tall= false
+
+puts tall
+
+#what is a nil
+husband = nil
+puts husband
+
